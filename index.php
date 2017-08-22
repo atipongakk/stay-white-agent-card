14de0c4faddba781f598371311bf68237e5c2df8
Atipong Akkarasa <atipongakk@gmail.com>
2017-08-22 22:42:55 +0700

    test

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-			$file = fopen('IMG_A.jpg','rb');
+			$file = fopen('IMG_A.jpg','w+');
 			fwrite($file,$result);
 			fclose($file);
 			
