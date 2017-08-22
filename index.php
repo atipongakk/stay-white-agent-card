38889ccbbc5d219099c8fd502c78359091f73497
nickpisit-office <chainningconstr9999@gmail.com>
2017-08-22 18:08:22 +0700

    add line bot

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
 <?php
-      echo "ABC";
+$access_token = 'W7YMx9jNYYZtS3bVAc3Guds4jYf2Fs3aSAVldxFKP5uurefxWboth0BZuT0YyTGvzlYjtS38ORyEbFBPu/Q8J4ZmMNqRcopzwIUXHwT9J4fKv1C8W9bM8uv4+7Z2J2kiX+nU5HT9TSADJVrR1uuUQwdB04t89/1O/w1cDnyilFU=';
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+$file = fopen("log.txt","w+");
+fwrite($file,$content);
+fclose($file);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		} 
+	}
+}
 ?>
