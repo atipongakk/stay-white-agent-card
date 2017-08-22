5b6fd3f66c8885b6c043f227b2e1d7dbca816be2
Atipong Akkarasa <atipongakk@gmail.com>
2017-08-22 23:19:20 +0700

    กหฟ

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'อัฟโหลดรูปเรียบร้อย\nโปรดระบุรายละเอียดข้อมูลเพิ่มเติมของเจ้าของรูป'
+				'text' => 'อัฟโหลดรูปบัตรเรียบร้อย!! โปรดระบุรายละเอียดข้อมูลเพิ่มเติมสำหรับเจ้าของบัตร'
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
