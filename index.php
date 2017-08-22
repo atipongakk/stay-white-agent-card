f30df3dde9c063853b8ad287be0fe6b267371465
nickpisit-office <chainningconstr9999@gmail.com>
2017-08-22 18:22:25 +0700

    dsads

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'https://lacphoto.org/berenice/wp-content/uploads/Test-Logo.svg.png'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
