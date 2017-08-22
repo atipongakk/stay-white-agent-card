1ddb1dd98817a99b2154076457a33c8800e99cb6
Atipong Akkarasa <atipongakk@gmail.com>
2017-08-22 23:10:11 +0700

    ทดสอบ

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'ทดสอบ'
+				'text' => 'อัฟโหลดรูปเรียบร้อย\nโปรดระบุรายละเอียดข้อมูลเพิ่มเติมของเจ้าของรูป'
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
